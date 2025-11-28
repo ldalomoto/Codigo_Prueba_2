4014ff8c174e4ae2ea4b7709adfc609c10314f08
ldalomoto <ldalomoto@uce.edu.ec>
2025-11-28 10:19:51 -0500

    controller

--- first-api-rest/src/main/java/com/first/api/controller/HelloController.java
+++ first-api-rest/src/main/java/com/first/api/controller/HelloController.java
@@ -11,34 +11,36 @@ public class HelloController {
     //Este es un nuevo comentario
     //Agregamos algo nuevo 2
     @GetMapping("/hello")
-    public String saludar(){
+    public String saludar() {
         return "Hola Mundo con REST en Spring Boot";
     }
 
     //Este es otro comentario
     //Agregamos algo 1
     @GetMapping("/hello2")
-    public String saludar2(){
+    public String saludar2() {
         return "Hola Mundo con REST en Spring Boot";
     }
-
 
     @GetMapping("/hello3")
-    public String saludar3(){
+    public String saludar3() {
         return "Hola Mundo con REST en Spring Boot";
     }
-
 
     @GetMapping("/hello4")
-    public String saludar4(){
+    public String saludar4() {
         return "Hola Mundo con REST en Spring Boot";
     }
-
 
     //Otro comentario
     @GetMapping("/hello6")
-    public String saludar6(){
+    public String saludar6() {
+        return "Hola Mundo con REST en Spring Boot";
+    }
+
+    //Otro comentario
+    @GetMapping("/hello7")
+    public String saludar7() {
         return "Hola Mundo con REST en Spring Boot";
     }
 }
-
